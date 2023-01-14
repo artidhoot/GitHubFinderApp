78cedd6fb8db89d448b62ce9518a5f53d75c62b5
Anamika Rathi <anamika.rathi1978@gmail.com>
2023-01-14 22:18:11 +0530

    update build for gh-pages

---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,46 @@
+import { createContext, useReducer } from "react";
+import { createRenderer } from "react-dom/test-utils";
+import githubReducer from "./GithubReducer";
+
+const GithubContext = createContext()
+
+export const GithubProvider = ({children}) => {
+   const initialState = {
+    users:[],
+    user:{},
+    repos:[],
+    loading:false
+   }
+
+   const [state, dispatch] = useReducer(githubReducer, initialState)
+
+   
+   // Get initial users (testing purposes)
+   /* const fetchUsers = async () => {
+        setLoading()
+
+        const response =   await fetch(`${GITHUB_URL}/users`, {
+            headers: {
+                Authorization: `token ${GITHUB_TOKEN}`
+            }
+        })
+    
+        const data = await response.json()
+        
+        dispatch({
+            type: 'GET_USERS',
+            payload: data,
+        })
+     }
+     */
+  
+     return <GithubContext.Provider value={{
+        ...state,
+        dispatch,
+        //fetchUsers,
+     }}>
+        {children}
+     </GithubContext.Provider>
+}
+
+export default GithubContext
